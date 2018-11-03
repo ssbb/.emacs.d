e6ad0aadaa40d7e21489ec9c522dd08f77f06eb2
Sviatoslav Bulbakha <mail@ssbb.me>
2018-11-03 23:58:47 +0300

    use M-* for winum, add elixir mode

--- init.el
+++ init.el
@@ -128,14 +128,42 @@
 
 ;; Window numbering
 (use-package winum
+  :no-require t
   :config
+  (setq winum-keymap
+	(let ((map (make-sparse-keymap)))
+	  (define-key map (kbd "C-`") 'winum-select-window-by-number)
+	  (define-key map (kbd "C-²") 'winum-select-window-by-number)
+	  (define-key map (kbd "M-0") 'winum-select-window-0-or-10)
+	  (define-key map (kbd "M-1") 'winum-select-window-1)
+	  (define-key map (kbd "M-2") 'winum-select-window-2)
+	  (define-key map (kbd "M-3") 'winum-select-window-3)
+	  (define-key map (kbd "M-4") 'winum-select-window-4)
+	  (define-key map (kbd "M-5") 'winum-select-window-5)
+	  (define-key map (kbd "M-6") 'winum-select-window-6)
+	  (define-key map (kbd "M-7") 'winum-select-window-7)
+	  (define-key map (kbd "M-8") 'winum-select-window-8)
+	  map))
+  (require 'winum)
   (winum-mode))
 
 ;;;; PROGRAMMING LANGUAGES
 
 ;; Web-mode
 (use-package web-mode
-  :mode "\\.html\\'")
+  :mode "\\.html\\'"
+  :config
+  (setq web-mode-markup-indent-offset 2
+	web-mode-css-indent-offset 2
+	web-mode-code-indent-offset 2))
+
+;; CSS & JS indent
+(setq js-indent-level 2)
+(setq css-indent-offset 2)
+
+;; Better RET
+(define-key global-map (kbd "RET") 'newline-and-indent)
+
 
 ;; Elm langauge
 (use-package elm-mode
@@ -146,6 +174,15 @@
   :after (elm-mode flycheck)
   :hook (flycheck-mode . flycheck-elm-setup))
 
+;; Elixir language
+(use-package elixir-mode
+  :init
+  (add-hook 'elixir-mode-hook (lambda () (add-hook 'before-save-hook 'elixir-format nil t))))
+
+
+(use-package alchemist
+  :after (elixir-mode))
+
 ;;;; CUSTOM FILE
 
 (custom-set-variables
@@ -155,7 +192,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (winum web-mode magit doom-modeline flycheck-color-mode-line flycheck-pos-tip flycheck-elm exec-path-from-shell flycheck elm-mode helm-projectile projectile helm color-theme-sanityinc-tomorrow use-package))))
+    (alchemist winum web-mode magit doom-modeline flycheck-color-mode-line flycheck-pos-tip flycheck-elm exec-path-from-shell flycheck elm-mode helm-projectile projectile helm color-theme-sanityinc-tomorrow use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
