4789b118118aeed27867e316297d3019aca35666
Sviatoslav Bulbakha <mail@ssbb.me>
2019-09-15 23:41:47 +0300

    add rainbow delimiters

---
+++ .custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (rainbow-delimiters yasnippet-snippets yaml-mode winum web-mode use-package undo-tree typescript-mode rjsx-mode reason-mode pyenv-mode-auto py-autopep8 prettier-js org-plus-contrib nginx-mode magit lsp-ui keyfreq hindent helm-rage helm-projectile helm-lsp helm-ag haskell-mode flycheck-pycheckers flycheck-pos-tip flycheck-elm flycheck-color-mode-line expand-region exec-path-from-shell evil-nerd-commenter emmet-mode elpy elm-mode doom-modeline company-quickhelp company-lsp color-theme-sanityinc-tomorrow alchemist))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
